4df908bb50023459869fddaf7f6b3c5fbfc6cc78
vinibrisola <vinibrisola2@gmail.com>
2022-08-15 20:50:08 -0300

    adjusted social media links

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -3,9 +3,9 @@ import { BsLinkedin, BsInstagram, BsFacebook } from 'react-icons/bs';
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a href="https://linkedin.com" target="_blank" rel="noreferrer"><BsLinkedin /></a>
-      <a href="https://github.com" target="_blank" rel="noreferrer"><BsFacebook /></a>
-      <a href="https://instagram.com" target="_blank" rel="noreferrer"><BsInstagram /></a>
+      <a href="https://www.linkedin.com/in/viniciusbrisola/" target="_blank" rel="noreferrer"><BsLinkedin /></a>
+      <a href="https://github.com/vinibrisola" target="_blank" rel="noreferrer"><BsFacebook /></a>
+      <a href="https://www.instagram.com/vinibrisa/" target="_blank" rel="noreferrer"><BsInstagram /></a>
     </div>
   )
 }
